0d382e8c02a41d0734fccd206ab4f77b3700e99e
basantaad817 <basantaad817@gmail.com>
2024-06-21 19:31:38 +0545

    profile and signup

---
+++ frontend/src/app/profile/page.jsx
@@ -0,0 +1,170 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import { useAuth } from "@/context/userContext";
+import ApiRequest from "@/utils/apiRequest";
+
+// server side rendering false or lazy loading to escpe error
+const Image = dynamic(() => import("next/image"), { ssr: false });
+const DashChangePassword = dynamic(
+  () => import("@/components/DashChangePassword"),
+  { ssr: false }
+);
+const DashExamHistory = dynamic(() => import("@/components/DashExamHistory"), {
+  ssr: false,
+});
+const DashProfile = dynamic(() => import("@/components/DashProfile"), {
+  ssr: false,
+});
+const PropagateLoader = dynamic(
+  () => import("react-spinners/PropagateLoader"),
+  { ssr: false }
+);
+
+const Profile = () => {
+  const { isAuthenticated, user, setUser } = useAuth();
+  const [activeTab, setActiveTab] = useState("profile");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        setLoading(true);
+        const res = await ApiRequest.get("/user/me");
+        setUser(res?.data?.data?.user);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setLoading(false);
+      }
+    };
+
+    if (isAuthenticated) {
+      fetchUserData();
+    }
+  }, [isAuthenticated, setUser]);
+
+  const handleImageChange = async (event) => {
+    const imageFile = event.target.files[0];
+    setSelectedImage(imageFile);
+
+    const formData = new FormData();
+    formData.append("avatar", imageFile);
+
+    try {
+      setLoading(true);
+      const response = await ApiRequest.patch("/user/avatar", formData);
+
+      setUser((prevUser) => ({
+        ...prevUser,
+        avatar: response?.data?.data?.user?.avatar,
+      }));
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="w-full h-full md:h-[736px] max-w-6xl px-5 mx-auto my-10 md:px-0">
+      {loading ? (
+        <div className="flex items-center justify-center h-[50%]">
+          <PropagateLoader color="#0eb1a6" loading={loading} size={20} />
+        </div>
+      ) : (
+        <div className="bg-[#F7F7F7] h-full  border-2 border-[#BBD6FF] rounded-xl flex flex-col md:flex-row">
+          {/* left div */}
+          <div className="w-[265px] h-full md:border-r-2 border-[#BBD6FF] pt-10 mx-auto">
+            {user && (
+              <div className="relative">
+                <Image
+                  src={user.avatar}
+                  alt="profile"
+                  height={129}
+                  width={129}
+                  className="w-[129px] h-[129px] rounded-full mx-auto object-cover"
+                />
+
+                <label
+                  htmlFor="profileImageInput"
+                  className="absolute transform translate-x-1/2 cursor-pointer bottom-6 right-1/2"
+                >
+                  <Image
+                    src="/profileIcon.png"
+                    alt="profile"
+                    width={32}
+                    height={32}
+                    className="duration-300 hover:scale-110"
+                  />
+                </label>
+                <input
+                  id="profileImageInput"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+                <p className="text-center text-[#0E0F0F] font-semibold pt-4">
+                  {user.fullname}
+                </p>
+              </div>
+            )}
+            <div className="mt-12">
+              <p
+                className={`px-6 py-3 cursor-pointer border-l-4  ${
+                  activeTab === "profile"
+                    ? "bg-[#E5EFFF] border-[#C40031]"
+                    : "border-transparent "
+                } `}
+                onClick={() => setActiveTab("profile")}
+              >
+                Profile
+              </p>
+
+              <p
+                className={`px-6 py-3 cursor-pointer border-l-4  ${
+                  activeTab === "changePassword"
+                    ? "bg-[#E5EFFF] border-[#C40031]"
+                    : "border-transparent"
+                }`}
+                onClick={() => setActiveTab("changePassword")}
+              >
+                Change Password
+              </p>
+
+              <p
+                className={`px-6 py-3 cursor-pointer border-l-4  ${
+                  activeTab === "exam-detail"
+                    ? "bg-[#E5EFFF] border-[#C40031]"
+                    : "border-transparent"
+                }`}
+                onClick={() => setActiveTab("exam-detail")}
+              >
+                Exam Details
+              </p>
+            </div>
+          </div>
+          {/* right div */}
+          <div className="md:flex-1">
+            <h1 className="text-[#0E0F0F] font-bold text-3xl md:text-5xl border-b-2 border-[#BBD6FF] w-full pt-10 pl-10 pb-8 tracking-wide">
+              {activeTab === "profile"
+                ? "Profile"
+                : activeTab === "changePassword"
+                ? "Change Password"
+                : "Exam History"}
+            </h1>
+            {/* details */}
+            {activeTab === "profile" && <DashProfile />}
+            {activeTab === "changePassword" && <DashChangePassword />}
+            {activeTab === "exam-detail" && <DashExamHistory />}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
---
+++ frontend/src/app/signup/page.jsx
@@ -0,0 +1,163 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation"; // Changed from 'next/navigation'
+import { useAuth } from "@/context/userContext";
+import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import { registerSchema } from "@/schemas";
+import ApiRequest from "@/utils/apiRequest";
+import PasswordStrengthBar from "react-password-strength-bar";
+
+const Signup = () => {
+  const { isAuthenticated, user, setIsAuthenticated } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      router.push("/profile");
+    }
+  }, []);
+
+  const initialValues = {
+    fullname: "",
+    email: "",
+    password: "",
+  };
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: registerSchema,
+      onSubmit: async (values) => {
+        try {
+          setIsLoading(true);
+          setErrorMessage("");
+          const res = await ApiRequest.post("/user/register", values);
+          // console.log(res);
+          setIsAuthenticated(true);
+          setIsLoading(false);
+          router.push("/profile");
+        } catch (error) {
+          setIsLoading(false);
+          setIsAuthenticated(false);
+          setErrorMessage(error.response?.data?.message);
+        }
+      },
+    });
+
+  return (
+    <div className="flex flex-col w-full gap-20 px-5 pb-40 mx-auto mt-8 md:flex-row md:px-0 md:mt-16 max-w-7xl">
+      {/* left div with image  */}
+      <div className="hidden md:inline">
+        <Image src="/login.png" alt="login" width={526} height={594} />
+      </div>
+      {/* right div with forms */}
+      <div className="w-[560px]">
+        <h1 className="text-xl italic font-bold md:text-3xl text-brown">
+          Hello User, you will have a great <br /> journey
+        </h1>
+        <form onSubmit={handleSubmit} className="mt-6">
+          <div className="flex flex-col h-20 px-5 py-3 bg-white border-l-4 border-transparent focus-within:border-primary ">
+            <label className="italic font-bold md:text-xl text-brown">
+              Full Name
+            </label>
+            <input
+              type="text"
+              className="outline-none pt-1 placeholder:font-bold placeholder:italic placeholder:text-[#122738]/40 md:placeholder:text-xl font-bold italic md:text-xl text-[#122738]"
+              placeholder="Your Name"
+              name="fullname"
+              value={values.fullname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          {errors.fullname && touched.fullname ? (
+            <p className="px-4 mt-2 text-lg italic text-red-500">
+              {errors.fullname}
+            </p>
+          ) : null}
+          <div className="flex flex-col h-20 px-5 py-3 bg-white border-l-4 border-transparent mt-9 focus-within:border-primary">
+            <label className="italic font-bold md:text-xl text-brown">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="outline-none pt-1 placeholder:font-bold placeholder:italic placeholder:text-[#122738]/40 md:placeholder:text-xl font-bold italic md:text-xl text-[#122738]"
+              placeholder="name@company.com"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          {errors.email && touched.email ? (
+            <p className="px-4 mt-2 text-lg italic text-red-500">
+              {errors.email}
+            </p>
+          ) : null}
+          <div className="flex flex-col h-20 px-5 py-3 bg-white border-l-4 border-transparent mt-9 focus-within:border-primary">
+            <label className="italic font-bold md:text-xl text-brown">
+              Password
+            </label>
+            <input
+              type="password"
+              className="outline-none pt-1 placeholder:font-bold placeholder:italic placeholder:text-[#122738]/40 md:placeholder:text-xl"
+              placeholder="****************"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {values.password && (
+              <PasswordStrengthBar password={values.password} />
+            )}
+          </div>
+          {errors.password && touched.password ? (
+            <p className="px-4 mt-2 text-lg italic text-red-500 ">
+              {errors.password}
+            </p>
+          ) : null}
+          {errorMessage ? (
+            <p className="px-4 mt-2 text-lg italic text-red-500">
+              {errorMessage}
+            </p>
+          ) : null}
+          <div className="mt-6 space-x-16">
+            <button
+              type="submit"
+              className="px-4 py-3 italic font-bold text-white duration-300 md:text-xl bg-primary hover:bg-primary/80"
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Signup"}
+            </button>
+            <Link href="/login">
+              <button className="px-4 py-3 md:text-xl italic font-bold bg-[#FFFFFF] text-[#122738] border-2 border-[#122738]">
+                Login
+              </button>
+            </Link>
+          </div>
+        </form>
+
+        {/* login with google and facebook */}
+        {/* <div className="flex items-center justify-between mt-6 md:mt-20">
+          <p className="italic font-bold md:text-xl text-brown">
+            or login with
+          </p>
+
+          <div className="flex gap-12">
+            <p className="text-[#122738] font-bold md:text-xl italic">
+              Facebook
+            </p>
+            <p className="text-[#122738] font-bold md:text-xl italic">Google</p>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
