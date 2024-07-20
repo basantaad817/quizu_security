fcb7ffd934a564531b42b207cfe0f33e32c993fd
basantaad817 <basantaad817@gmail.com>
2024-07-20 18:42:29 +0545

    utils working made

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+      folder: "quizu",
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export const deleteFromCloudinary = async (publicId) => {
+  try {
+    console.log(publicId);
+    const result = await cloudinary.uploader.destroy(`quizu/${publicId}`, {
+      type: "upload",
+      resource_type: "image",
+    });
+    console.log("Deleted Successfully:", result);
+    return result;
+  } catch (error) {
+    console.error("Error while deleting old avatar:", error);
+    throw new AppError("Error while deleting old avatar!", 500);
+  }
+};
